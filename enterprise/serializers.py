4e6d635bd02ebeca7ac50942e9ae22952b77a0af
沉辰尘 <1194696374@qq.com>
2022-05-27 10:08:38 +0800

    人才检索和岗位检索

--- enterprise/serializers.py
+++ enterprise/serializers.py
@@ -6,10 +6,11 @@
 """
 from rest_framework import serializers
 from enterprise import models
+from cv import models as cv_models
+from user.models import User
 
 
 class PositionClassSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = models.PositionClass
         fields = ("id", "name")
@@ -30,7 +31,45 @@ class PositionListSerializer(serializers.ModelSerializer):
 
 
 class PositionDetailSerializer(PositionListSerializer):
-
     class Meta:
         model = models.Position
         fields = "__all__"
+
+
+# 简历筛选
+class PersonnelRetrievalForeignSerializer(serializers.ModelSerializer):
+    # 外键序列化处理
+
+    class Meta:
+        user_id = serializers.CharField(source='User')
+        industry = serializers.CharField(source='enterprise.Field')
+        fields = ("user_id", "industry")
+
+
+class PersonnelRetrievalSerializer(PersonnelRetrievalForeignSerializer):
+    # 简历筛选
+    class Meta:
+        model = cv_models.CV
+        fields = "__all__"
+
+
+# 简历筛选前端传入参数校验
+class PersonnelRetrievalDataSerializer(serializers.Serializer):
+    Search_term = serializers.CharField(required=True, help_text='检索词')
+    industry = serializers.CharField(source='enterprise.Field', required=False, help_text='目标行业领域')
+    major = serializers.CharField(required=False, help_text='所学专业名称')
+    courses = serializers.CharField(required=False, help_text='所修课程')
+    english_skill = serializers.IntegerField(required=False, help_text='英语技能水平')
+    computer_skill = serializers.IntegerField(required=False, help_text='计算机技能水平')
+    expected_salary = serializers.IntegerField(required=False, help_text='最低期望薪资')
+    professional_skill = serializers.CharField(required=False, help_text='掌握专业技能')
+
+
+# 岗位筛选前端传入参数校验
+class PositionDataSerializer(serializers.Serializer):
+    Search_term = serializers.CharField(required=True, help_text='检索词')
+    enterprise = serializers.CharField(source='models.CASCADE', required=False, help_text='企业')
+    pst_class = serializers.CharField(source='PositionClass', required=False, help_text='岗位类别')
+    fullname = serializers.CharField(required=False, help_text='岗位扩展名称（别称，默认为岗位类别）')
+    job_content = serializers.CharField(required=False, help_text='工作内容')
+    requirement = serializers.CharField(required=False, help_text='岗位基本要求')
