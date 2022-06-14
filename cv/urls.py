baa9be4731ba1d73282674da839a8ae08d04e267
沉辰尘 <1194696374@qq.com>
2022-06-14 15:38:56 +0800

    重构简历部分三个接口

--- cv/urls.py
+++ cv/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<int:user_id>/<int:cv_id>/show-cv/', views.show_cv, name='show_cv'),
     path('<int:user_id>/<int:cv_id>/download-cv/', views.save_as_pdf, name='download-cv'),
     path('search_cv_page/', views.SearchCv.as_view(), name='search_cv_page'),
+    path('cv_detail/', views.CvDetail.as_view(), name='cv_detail'),
+    path('cv_deliver/',views.CvDeliver.as_view(), name='cv_deliver'),
+    path('cv_download_pdf/',views.MakePdf.as_view(), name='cv_download_pdf')
 ]
